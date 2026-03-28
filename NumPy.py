fb6716dcb1f8552ccb098ec2c4eade21ac7d328e
OmjiTripathi28 <omji.tripathi.28@gmail.com>
2026-03-28 10:26:20 +0530

    added new questions

--- NumPy.py
+++ NumPy.py
@@ -55,3 +55,23 @@ Create an array: [1, 2, 3]
 f = np.array([[1, 2, 3]])
 print(f.astype(float))
 
+#CHAPTER 2 LEVEL 1: ARRAY CREATION (REAL NUMPY STARTS)
+"""Q1
+
+Create a 3×3 matrix of zeros (int type)
+
+Q2
+
+Create an array from 1 to 15 with step 2
+
+Q3
+
+Create 5 equally spaced numbers between 10 and 50
+
+Q4
+
+Create a 4×4 identity matrix
+
+Q5 🔥
+
+Create a 2×3 matrix with random integers between 10 and 50"""
